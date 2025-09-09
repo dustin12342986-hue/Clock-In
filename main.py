80a4a8be9e08577656921fcceb7af89abe5c19fc
dustin12342986-hue <dustin12342986@gmail.com>
2025-09-08 19:50:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True  # <-- add this line
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
 import os
 import discord
 from discord import app_commands
